6ae82cc3b1273e94b8f90f78619b30b716bdb5be
Clarke Lamothe <clamothe@intermediait.com>
2022-08-04 14:17:52 -0300

    Splash implemented

--- app/src/main/java/com/clarkelamothe/marvelapp/MainActivity.kt
+++ app/src/main/java/com/clarkelamothe/marvelapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.clarkelamothe.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Handle the splash screen transition.
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
