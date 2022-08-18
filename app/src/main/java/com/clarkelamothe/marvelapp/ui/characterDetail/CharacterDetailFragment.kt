26c092975bdd3dc869e10dd23e1ee676aa73a3d1
Clarke Lamothe <clamothe@intermediait.com>
2022-08-18 17:00:49 -0300

    removing extra spaces and renaming navigation file characterDetail fragment

--- app/src/main/java/com/clarkelamothe/marvelapp/ui/characterDetail/CharacterDetailFragment.kt
+++ app/src/main/java/com/clarkelamothe/marvelapp/ui/characterDetail/CharacterDetailFragment.kt
@@ -1,4 +1,4 @@
-package com.clarkelamothe.marvelapp.ui.characterDetails
+package com.clarkelamothe.marvelapp.ui.characterDetail
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.clarkelamothe.marvelapp.R
 
-class CharacterDetailsFragment : Fragment() {
+class CharacterDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_details, container, false)
+        return inflater.inflate(R.layout.fragment_character_detail, container, false)
     }
 }
